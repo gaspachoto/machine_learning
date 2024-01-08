a73c3aa3c798a9be04717e1b5ad6cb21bc11d70b
vlad.iliev <vlad.iliev@abv.bg>
2024-01-08 16:27:34 +0200

    up to date

--- test/task1.py
+++ test/task1.py
@@ -1,8 +1,9 @@
-def factorial(n):
-    if n == 1:
-        return n
-    else:
-        return n*factorial(n-1)
+def fact(n):
+    if n in (0, 1):
+        return 1
+
+    n_1_fact = fact(n - 1)
+    return n * n_1_fact
 
 
-num = 7
+print(fact(52))
